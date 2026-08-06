5cd5541a18e8be16efc20e2ef9f28fbbe4564bdb
Lily Dietrich <lilydietrich@Lilys-MacBook-Pro.local>
2026-08-05 20:11:43 -0500

    Prepare Resonance 1.1.4

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "mov.unblocked.resonance"
         minSdk = 26
         targetSdk = 36
-        versionCode = 14
-        versionName = "1.1.3"
+        versionCode = 15
+        versionName = "1.1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
